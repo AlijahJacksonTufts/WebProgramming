8cfd95ad43ca2a49bb055ab1a4650999b7ad96f4
alijah1696 <alijah1696@gmail.com>
2026-02-08 14:14:47 -0500

    u

--- hw2/script.js
+++ hw2/script.js
@@ -1,2 +1,20 @@
-console.log('Script loaded successfully.');
+let treeClicked = true
+
+window.addEventListener('load', () => {
+    const treeLottie = document.getElementById('treeLottie').dotLottie;
+    const treeWrapper = document.getElementById('treeLottie').parentElement;
+
+    document.addEventListener('click', async () => {
+        if (treeLottie && treeClicked) {
+            treeLottie.play();
+            treeClicked = false;
+
+            treeWrapper.classList.add('tree-zoom');
+            await wait(300);
+            treeWrapper.classList.add('tree-move');
+        }
+    }); 
+});
+
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
