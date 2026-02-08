c717f589893e1bf00371ada82771844ad34e800a
alijah1696 <alijah1696@gmail.com>
2026-02-08 13:07:01 -0500

    test

--- hw2/script.js
+++ hw2/script.js
@@ -1,10 +1,2 @@
 console.log('Script loaded successfully.');
 
-window.addEventListener('load', () => {
-    const lottieElement = document.getElementById('myLottie');
-    document.addEventListener('click', () => {
-        if (lottieElement) {
-            lottieElement.play();
-        }
-    }, { once: true }); 
-});
